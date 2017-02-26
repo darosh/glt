68978be89d400ed4cf108d5781c5466e357d324f
Jan Forst <ensonador@gmail.com>
2017-02-26 15:21:55 +0100

    example generator graph format

--- components/glt/src/generator/exampleGenerators.js
+++ components/glt/src/generator/exampleGenerators.js
@@ -1,51 +1,49 @@
 import {features} from '../features/index';
-import {VALUE} from '../features/names';
+import {PARAM, VALUE} from '../features/names';
 
 export function exampleGenerators() {
     const ret = [];
     const keys = Object.keys(features.generator);
 
     for (let i = 0; i < keys.length; i++) {
-        ret.push(getGenerator(keys[i]));
-
+        ret.push(getGenerator(keys[i], keys[i]));
         const examples = features.generator[keys[i]].examples || [];
 
         for (let j = 0; j < examples.length; j++) {
-            ret.push(getGenerator(keys[i], examples[j], j + 1));
+            ret.push(getGenerator(keys[i], keys[i], examples[j], j + 1));
         }
     }
 
     return ret;
 }
 
-function getGenerator(name, params = null, index = 0) {
+function getGenerator(name, title, params = null, index = 0) {
     const generator = features.generator[name];
     const deps = ['Length', 'SinX', 'SinY'];
     const ret = {
-        title: name,
+        title: title ? title : undefined,
         example: index ? index.toString() : undefined,
         data: [
-            {
-                generator: name,
-                params: params ? params : (!generator.params ? [] : generator.params.map((param) => {
-                    return param[1] === VALUE ? getGenerator(deps.shift()) : param[2];
-                }))
-            }
+            [
+                name,
+                ...(params ? params : (!generator.params ? [] : generator.params
+                    .filter((param) => !param[PARAM.placeholder])
+                    .map((param) => {
+                        return param[1] === VALUE ? getGenerator(deps.shift()) : param[2];
+                    })))
+            ]
         ]
     };
 
-    if (generator.scale) {
-        ret.data.push({
-            transform: 'Scale',
-            params: [[generator.scale, generator.scale]]
-        });
+    if (generator.scale && (generator.scale !== 1)) {
+        ret.data.push([
+            'Scale',
+            [generator.scale, generator.scale]
+        ]);
     }
 
     if (generator.normalized === false) {
-        ret.data.push(
-            {
-                modify: 'Normalize'
-            })
+        ret.data.push(['Normalize']);
     }
 
     return ret;
