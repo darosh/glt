718bea3b7a0dd91c640b254377f74923cbea7827
Jan Forst <ensonador@gmail.com>
2017-02-28 00:23:58 +0100

    show code settings

--- components/glt-demo/app/pipes/cjson.pipe.js
+++ components/glt-demo/app/pipes/cjson.pipe.js
@@ -7,6 +7,7 @@
             transform: (value, i, l) => {
                 let v = glt.deep(value);
                 delete v.title;
+                delete v.example;
                 return CJSON(v, {indent: i, maxLength: Math.max(32, Math.round(l))});
             }
         });
--- components/glt-demo/app/services/app.service.js
+++ components/glt-demo/app/services/app.service.js
@@ -6,19 +6,23 @@
                 this.canvas = document.createElement('canvas');
                 this.renderer = new glt.Renderer(this.canvas).size(128);
 
-                this.previewSizes = [[256, 256], [192, 192], [128, 128], [64, 64]];
+                this.previewSizes = [[64, 64], [128, 128], [192, 192], [256, 256]];
                 this.previewSize = this.previewSizes[2];
 
-                this.thumbnailSizes = [[256, 256], [192, 192], [128, 128], [64, 64]];
-                this.thumbnailSize = this.thumbnailSizes[2];
+                this.thumbnailSizes = [[64, 64], [128, 128], [192, 192], [256, 256]];
+                this.thumbnailSize = this.thumbnailSizes[1];
 
-                this.renderSizes = [[256, 256], [192, 192], [128, 128], [64, 64]];
+                this.renderSizes = [[64, 64], [128, 128], [192, 192], [256, 256]];
                 this.renderSize = this.renderSizes[2];
 
                 this.previewQualities = [0.5, 1, 2, 4, 8];
                 this.previewQuality = this.previewQualities[1];
 
                 this.randomItems = 78;
+                this.show = {
+                    code: false,
+                    time: false
+                };
 
                 var queue = this.queue = [];
                 var pending = 0;
