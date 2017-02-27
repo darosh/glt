289e3b8d22c785bed51a6a48b4e86884677c7ae8
Jan Forst <ensonador@gmail.com>
2017-02-27 22:58:19 +0100

    code preview layout

--- components/glt-demo/app/pipes/cjson.pipe.js
+++ components/glt-demo/app/pipes/cjson.pipe.js
@@ -7,7 +7,7 @@
             transform: (value, i, l) => {
                 let v = glt.deep(value);
                 delete v.title;
-                return CJSON(v, {indent: i, maxLength: l});
+                return CJSON(v, {indent: i, maxLength: Math.max(32, Math.round(l))});
             }
         });
 })(window.app || (window.app = {}));
