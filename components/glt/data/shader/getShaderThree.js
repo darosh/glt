7774b1a6016b06befc292887051763722aff76ab
Jan Forst <ensonador@gmail.com>
2017-03-01 11:00:59 +0100

    partials shader

--- components/glt/data/shader/getShaderThree.js
+++ components/glt/data/shader/getShaderThree.js
@@ -1,12 +1,27 @@
-import {POS} from '../../features/names';
+import {POS, COORD} from '../../features/names';
+import {isArray} from '../../utils/isArray';
 
 export function getShaderThree(shader, lib, uniforms) {
+    let shaderLines;
+
+    if (isArray(shader)) {
+        const L = shader.length;
+        shaderLines = [
+            'int part = int(floor(((' + POS + '.x+1.)/2.)*' + L + '.)/' + L + '.);',
+            POS + '.x = ((' + POS + '.x+1.)*' + L + '.)%2.-1.',
+            'vec3 color;'
+        ];
+        shader.forEach((s, i) => {
+            shaderLines.push((i ? 'else ' : '') + 'if(part == ' + i + ') { color = ' + s + '; }');
+        });
+    }
+
     return {
         vertexShader: [
-            'varying vec2 ' + POS + 'a;',
+            'varying vec2 ' + COORD + ';',
             '',
             'void main() {',
-            ' ' + POS + 'a = uv * 2. - 1.;',
+            ' ' + COORD + ' = uv * 2. - 1.;',
             ' gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);',
             '}'
         ].join('\n'),
@@ -16,11 +31,11 @@ export function getShaderThree(shader, lib, uniforms) {
             uniforms ? '' : null,
             lib,
             '',
-            'varying vec2 ' + POS + 'a;',
+            'varying vec2 ' + COORD + ';',
             '',
             'void main() {',
-            ' vec2 pos = vec2(posa.x, posa.y * resolution.y /resolution.x);',
-            ' vec3 color = ' + shader + ';',
+            ' vec2 ' + POS + ' = vec2(' + COORD + '.x, ' + COORD + '.y * resolution.y /resolution.x);',
+            ...(shaderLines ? shaderLines : [' vec3 color = ' + shader + ';']),
             ' gl_FragColor = vec4(color, 1.0);',
             '}'
         ].filter((l) => l !== null).join('\n')
