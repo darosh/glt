3e707df4224bc247d4e4bc81bf531ecc4ea33abf
Jan Forst <ensonador@gmail.com>
2017-03-02 20:36:04 +0100

    contextual menu

--- components/glt-app/src/app/menu/menu.component.ts
+++ components/glt-app/src/app/menu/menu.component.ts
@@ -4,10 +4,12 @@ import {ConfigService} from '../services/config.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  inputs: ['menu']
 })
 export class MenuComponent {
   config;
+  menu;
 
   constructor(config: ConfigService) {
     this.config = config;
--- components/glt-app/src/app/services/config.service.ts
+++ components/glt-app/src/app/services/config.service.ts
@@ -7,14 +7,19 @@ export class ConfigService {
   randomItems = 78;
   thumbnailSizes = [[64, 64], [128, 32], [128, 128], [192, 48], [192, 192], [256, 64], [256, 256], [384, 96]];
   thumbnailSize = this.thumbnailSizes[2];
-  editSize = [512, 512];
+  editSizes = [[256, 256], [384, 384], [512, 512], [640, 640], [768, 768]];
+  editSize = this.editSizes[2];
   thumbnailQualities = [0.5, 1, 2, 4, 8];
+  editQualities = [0.5, 1, 2, 4, 8];
   thumbnailQuality = this.thumbnailQualities[1];
+  editQuality = this.editQualities[1];
+
   show = {
     code: false,
     time: false,
     partials: false
   };
+
   source = glt.samplesDemo[0];
   original = glt.samplesDemo[0];
 }
