5f7227a1aad03b35270f2c501d956f6bb55aa855
Jan Forst <ensonador@gmail.com>
2017-03-24 09:40:39 +0100

    glt-vue

--- components/glt-vue/src/services/config.js
+++ components/glt-vue/src/services/config.js
@@ -8,8 +8,8 @@ class ConfigService {
   editSizeIndex = 0
   editSize = this.editSizes[this.editSizeIndex]
 
-  thumbnailSizes = [[64, 64], [128, 32], [128, 128], [192, 48], [192, 192], [256, 64], [256, 256], [384, 96]]
-  thumbnailSizeIndex = 2
+  thumbnailSizes = [[128, 32], [128, 128], [192, 48], [192, 192], [256, 64], [256, 256]]
+  thumbnailSizeIndex = 1
   thumbnailSize = this.thumbnailSizes[this.thumbnailSizeIndex]
 
   histogramDark = false
